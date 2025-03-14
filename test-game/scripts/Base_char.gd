24fb33c944efd63187a11ba38759e9bbc91f818f
Sam-Sammich <seigelsamuel@gmail.com>
2025-03-14 12:51:58 -0500

    added base characters and restructured

---
+++ test-game/scripts/Base_char.gd
@@ -0,0 +1,73 @@
+extends CharacterBody2D
+
+
+const SPEED = 60.0
+const JUMP_VELOCITY = -110.0
+
+@export var animated_sprite: AnimatedSprite2D
+@export var input_char: String
+@onready var coyote_timer: Timer = $CoyoteTimer
+
+var has_double_jump := true
+
+
+func _physics_process(delta: float) -> void:
+	
+	# Add the gravity and reset double jump when on ground
+	if not is_on_floor():
+		velocity += get_gravity()*.3 * delta
+	else:
+		has_double_jump = true
+	
+	
+
+	# Handle jump.
+	if Input.is_action_just_pressed(input_char + "jump"):
+		if is_on_floor() or !coyote_timer.is_stopped():
+			velocity.y = JUMP_VELOCITY
+			coyote_timer.stop()
+		elif has_double_jump:
+			velocity.y = JUMP_VELOCITY * 0.8
+			has_double_jump = false
+
+	# Get the input direction and handle the movement/deceleration.
+	var direction := Input.get_axis(input_char + "left", input_char + "right")
+	# face move direction
+	if direction > 0:
+		animated_sprite.flip_h = false
+	elif direction < 0:
+		animated_sprite.flip_h = true
+	
+	
+	
+	
+	# Play animations
+	move_and_slide()
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.play("idle")
+		else:
+			animated_sprite.play("run")
+	else:
+		if has_double_jump:
+			if velocity.y > 0:
+				animated_sprite.play("fall")
+			else:
+				animated_sprite.play("jump")
+		else:
+			animated_sprite.play("roll")
+	
+	
+	
+	
+	# apply movement
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+
+	var was_on_floor = is_on_floor()
+	move_and_slide()
+	if is_on_floor() != was_on_floor && velocity.y >= 0:
+		coyote_timer.start()
