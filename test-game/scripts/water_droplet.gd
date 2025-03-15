0e64b88882a55e6f6cd13af080b97d99b18bf383
Sam-Sammich <seigelsamuel@gmail.com>
2025-03-14 21:29:58 -0500

    fixed wter death

--- test-game/scripts/water_droplet.gd
+++ test-game/scripts/water_droplet.gd
@@ -1,9 +1,12 @@
 extends Node2D
 
+@onready var timer: Timer = $Killzone/Timer
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
+		
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -13,4 +16,8 @@ func _physics_process(delta: float) -> void:
 
 
 func _on_killzone_body_entered(body: Node2D) -> void:
-	queue_free()
+	if !body.is_in_group("Player"):
+		hide()
+		if Engine.time_scale == 1:
+			queue_free()
+		
